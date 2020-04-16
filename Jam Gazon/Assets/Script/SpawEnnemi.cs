6bc113e09dc81f99b77e0e364fd5326227e93954
Albane <molliere97@gmal.com>
2020-04-16 10:53:55 +0200

    Spawn Ennemi + TaupePrefab

---
+++ Jam Gazon/Assets/Script/SpawEnnemi.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawEnnemi : MonoBehaviour
+{
+    [SerializeField]private GameObject[] TaupeSpawn;
+    [SerializeField] private float TimeSpawn = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnTaupe",TimeSpawn,5);
+    }
+
+    Vector2 RandomPos()
+    {
+        float X = Random.Range(-8, 9);
+        float Y = Random.Range(6,-10);
+
+        Vector2 newPos = new Vector2(X, Y);
+        return newPos;
+
+    }
+
+    void SpawnTaupe()
+    {
+        Instantiate(TaupeSpawn[Random.Range(0, 1)], RandomPos(), Quaternion.identity);
+    }
+}
